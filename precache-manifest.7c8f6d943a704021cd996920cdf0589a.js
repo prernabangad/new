b032953a21504331a01321e7c2a3ce97c4114be3
prernabangad <email address>
2020-09-03 20:01:59 +0530

    Updates

---
+++ precache-manifest.7c8f6d943a704021cd996920cdf0589a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83dd51fc155486fc5dc775598dd0c3f5",
+    "url": "/new/index.html"
+  },
+  {
+    "revision": "7a1c078acda8fd322293",
+    "url": "/new/static/css/main.ec656cc4.chunk.css"
+  },
+  {
+    "revision": "7f2bd373e8cd6a78ecf8",
+    "url": "/new/static/js/2.d2a67945.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/new/static/js/2.d2a67945.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a1c078acda8fd322293",
+    "url": "/new/static/js/main.578a97fe.chunk.js"
+  },
+  {
+    "revision": "a5b476f199478bb50791",
+    "url": "/new/static/js/runtime-main.37dffbe0.js"
+  }
+]);
